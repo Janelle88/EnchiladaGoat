04727f335ba22a79f276afa0fa7f4aed77f63d33
Janelle88 <jnchristensen1788@gmail.com>
2021-02-02 15:27:24 -0800

    added GoatsEatEnchiladas function

---
+++ R/GoatsEatEnchilada.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param goat
+#' @param enchilada
+#'
+#' @return
+#' @export
+#'
+#' @examples
+GoatsEatEnchilada <- function(goat, enchilada) {
+  (goat*52 + enchilada^5)/ enchilada^3
+}
